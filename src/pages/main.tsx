569efe9c33309f3038bbfc0d02e6d36bc3771446
yamokopatel <cvetkovsdeniss4@gmail.com>
2025-12-16 11:09:31 +0200

    Improved mistakes
    Improved previous commit mistakes.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -73,136 +73,136 @@ export default function Reductor(){
 				<h3>Properties</h3>
 				<div id="arrowProp" className="prop" style={{display: "none"}}>
 					<label>Start X:</label>
-					<input type="number" id="startX">
+					<input type="number" id="startX" />
 					<label>Start Y:</label>
-					<input type="number" id="startY">
-					<hr>
+					<input type="number" id="startY" />
+					<hr />
 					<label>End X:</label>
-					<input type="number" id="endX">
+					<input type="number" id="endX" />
 					<label>End Y:</label>
-					<input type="number" id="endY">
-					<hr>
+					<input type="number" id="endY" />
+					<hr />
 					<label>Color:</label>
-					<input type="color" id="lineColor">
+					<input type="color" id="arrowColor" />
 					<label>Width:</label>
-					<input type="number" id="lineWidth">
-					<hr>
-					<button>Apply</button>
+					<input type="number" id="arrowWidth" />
+					<hr />
+					<button id="applyArrow">Apply</button>
 				</div>
 				<div id="squareProp" className="prop" style={{display: "none"}}>
 					<label>X:</label>
-					<input type="number" id="coordinateX">
+					<input type="number" id="squareCoordinateX" />
 					<label>Y:</label>
-					<input type="number" id="coordinateY">
-					<hr>
+					<input type="number" id="squareCoordinateY" />
+					<hr />
 					<label>Width:</label>
-					<input type="number" id="figureWidth">
+					<input type="number" id="squareWidth" />
 					<label>Heigth:</label>
-					<input type="number" id="figureHeight">
-					<hr>
+					<input type="number" id="squareHeight" />
+					<hr />
 					<label>Line color:</label>
-					<input type="color" id="lineColor">
+					<input type="color" id="squareLineColor" />
 					<label>Line width:</label>
-					<input type="number" id="lineWidth">
-					<hr>
+					<input type="number" id="squareLineWidth" />
+					<hr />
 					<label>Fill color:</label>
-					<input type="color" id="fillColor">
+					<input type="color" id="squareFillColor" />
 					<label>Transparency</label>
-					<input type="number" id="fillTransparency" min="0" max="100">
-					<hr>
-					<button>Apply</button>
+					<input type="number" id="squareFillTransparency" min="0" max="100" />
+					<hr />
+					<button id="applySquare">Apply</button>
 				</div>
 				<div id="triangleProp" className="prop" style={{display: "none"}}>
 					<label>X:</label>
-					<input type="number" id="coordinateX">
+					<input type="number" id="triangleCoordinateX" />
 					<label>Y:</label>
-					<input type="number" id="coordinateY">
-					<hr>
+					<input type="number" id="triangleCoordinateY" />
+					<hr />
 					<label>Width:</label>
-					<input type="number" id="figureWidth">
+					<input type="number" id="triangleWidth" />
 					<label>Heigth:</label>
-					<input type="number" id="figureHeight">
-					<hr>
+					<input type="number" id="triangleHeight" />
+					<hr />
 					<label>Line color:</label>
-					<input type="color" id="lineColor">
+					<input type="color" id="triangleLineColor" />
 					<label>Line width:</label>
-					<input type="number" id="lineWidth">
-					<hr>
+					<input type="number" id="triangleLineWidth" />
+					<hr />
 					<label>Fill color:</label>
-					<input type="color" id="fillColor">
+					<input type="color" id="triangleFillColor" />
 					<label>Transparency</label>
-					<input type="number" id="fillTransparency" min="0" max="100">
-					<hr>
-					<button>Apply</button>
+					<input type="number" id="triangleFillTransparency" min="0" max="100" />
+					<hr />
+					<button id="applyTriangle">Apply</button>
 				</div>
 				<div id="pentagonProp" className="prop" style={{display: "none"}}>
 					<label>X:</label>
-					<input type="number" id="coordinateX">
+					<input type="number" id="pentagonCoordinateX" />
 					<label>Y:</label>
-					<input type="number" id="coordinateY">
-					<hr>
+					<input type="number" id="pentagonCoordinateY" />
+					<hr />
 					<label>Width:</label>
-					<input type="number" id="figureWidth">
+					<input type="number" id="pentagonWidth" />
 					<label>Heigth:</label>
-					<input type="number" id="figureHeight">
-					<hr>
+					<input type="number" id="pentagonHeight" />
+					<hr />
 					<label>Line color:</label>
-					<input type="color" id="lineColor">
+					<input type="color" id="pentagonLineColor" />
 					<label>Line width:</label>
-					<input type="number" id="lineWidth">
-					<hr>
+					<input type="number" id="pentagonLineWidth" />
+					<hr />
 					<label>Fill color:</label>
-					<input type="color" id="fillColor">
+					<input type="color" id="pentagonFillColor" />
 					<label>Transparency</label>
-					<input type="number" id="fillTransparency" min="0" max="100">
-					<hr>
-					<button>Apply</button>
+					<input type="number" id="pentagonFillTransparency" min="0" max="100" />
+					<hr />
+					<button id="applyPentagon">Apply</button>
 				</div>
 				<div id="hexagonProp" className="prop" style={{display: "none"}}>
 					<label>X:</label>
-					<input type="number" id="coordinateX">
+					<input type="number" id="hexagonCoordinateX" />
 					<label>Y:</label>
-					<input type="number" id="coordinateY">
-					<hr>
+					<input type="number" id="hexagonCoordinateY" />
+					<hr />
 					<label>Width:</label>
-					<input type="number" id="figureWidth">
+					<input type="number" id="hexagonWidth" />
 					<label>Heigth:</label>
-					<input type="number" id="figureHeight">
-					<hr>
+					<input type="number" id="hexagonHeight" />
+					<hr />
 					<label>Line color:</label>
-					<input type="color" id="lineColor">
+					<input type="color" id="hexagonLineColor" />
 					<label>Line width:</label>
-					<input type="number" id="lineWidth">
-					<hr>
+					<input type="number" id="hexagonLineWidth" />
+					<hr />
 					<label>Fill color:</label>
-					<input type="color" id="fillColor">
+					<input type="color" id="hexagonFillColor" />
 					<label>Transparency</label>
-					<input type="number" id="fillTransparency" min="0" max="100">
-					<hr>
-					<button>Apply</button>
+					<input type="number" id="hexagonFillTransparency" min="0" max="100" />
+					<hr />
+					<button id="applyHexagon">Apply</button>
 				</div>
 				<div id="circleProp" className="prop" style={{display: "none"}}>
 					<label>X:</label>
-					<input type="number" id="coordinateX">
+					<input type="number" id="circleCoordinateX" />
 					<label>Y:</label>
-					<input type="number" id="coordinateY">
-					<hr>
+					<input type="number" id="circleCoordinateY" />
+					<hr />
 					<label>Width:</label>
-					<input type="number" id="figureWidth">
+					<input type="number" id="circleWidth" />
 					<label>Heigth:</label>
-					<input type="number" id="figureHeight">
-					<hr>
+					<input type="number" id="circleHeight" />
+					<hr />
 					<label>Line color:</label>
-					<input type="color" id="lineColor">
+					<input type="color" id="circleLineColor" />
 					<label>Line width:</label>
-					<input type="number" id="lineWidth">
-					<hr>
+					<input type="number" id="circleLineWidth" />
+					<hr />
 					<label>Fill color:</label>
-					<input type="color" id="fillColor">
+					<input type="color" id="circleFillColor" />
 					<label>Transparency</label>
-					<input type="number" id="fillTransparency" min="0" max="100">
-					<hr>
-					<button>Apply</button>
+					<input type="number" id="circleFillTransparency" min="0" max="100" />
+					<hr />
+					<button id="applyCircle">Apply</button>
 				</div>
 			</div>
 		</div>
